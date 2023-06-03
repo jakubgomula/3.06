be08c26309a329805eda4f84f7883244de02db83
jakubgomula <135388410+jakubgomula@users.noreply.github.com>
2023-06-03 18:42:46 +0200

    Add files via upload

---
+++ zadanie 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj pierwszą liczbę: ");
+        double liczba1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Podaj drugą liczbę: ");
+        double liczba2 = double.Parse(Console.ReadLine());
+
+        if (liczba1 > liczba2)
+        {
+            Console.WriteLine("Większa liczba to: " + liczba1);
+        }
+        else if (liczba2 > liczba1)
+        {
+            Console.WriteLine("Większa liczba to: " + liczba2);
+        }
+        else
+        {
+            Console.WriteLine("Podane liczby są równe.");
+        }
+    }
+}
