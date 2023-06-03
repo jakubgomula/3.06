0ce6fbcc0b8d152b6c24a934db736675f8c79cb2
jakubgomula <135388410+jakubgomula@users.noreply.github.com>
2023-06-03 18:23:17 +0200

    Add files via upload

---
+++ zadanie 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double x, y, a, b, c, d;
+
+        Console.WriteLine("Podaj a");
+        a = inputValue();
+        Console.WriteLine("Podaj b");
+        b = inputValue();
+        Console.WriteLine("Podaj c");
+        c = inputValue();
+        Console.WriteLine("Podaj d");
+        d = inputValue();
+
+        if (b == 0 || d == 0)
+        {
+            Console.WriteLine("Brak rozwiązania");
+        }
+        else
+        {
+            x = ((a * d) + (b * c));
+            y = (b * d);
+            Console.WriteLine("x=" + x);
+            Console.WriteLine("y=" + y);
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
